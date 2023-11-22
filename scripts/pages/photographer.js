161d11c8f93bcf18656a946efb435ac84cf5f254
pierregaly33 <galy.pierre31@gmail.com>
2023-11-22 16:55:38 +0100

    affichage page d'accueil

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,15 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographerId() {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    console.log(id);
+
+    const photographersData = await fetch("../data/photographers.json").then((data) => data.json());
+    const photographers = photographersData.photographers;
+    console.log(photographers);
+    console.log(photographersData);
+
+    const photographer = photographers.find((photographer) => photographer.id == id);
+    console.log(photographer);
+}
+getPhotographerId();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -11,13 +11,16 @@ function photographerTemplate(data) {
         const aPrice = document.createElement("p");
         const h2 = document.createElement("h2");
         const h3 = document.createElement("h3");
+        const lien = document.createElement("a");
+        const url = `./photographer.html?id=${id}`;
 
         //Affichage des éléments
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(aTagline);
-        article.appendChild(aPrice);
+        article.appendChild(lien);
+        lien.appendChild(img);
+        lien.appendChild(h2);
+        lien.appendChild(h3);
+        lien.appendChild(aTagline);
+        lien.appendChild(aPrice);
 
         //Attributs des éléments
         img.setAttribute("src", picture);
@@ -26,6 +29,8 @@ function photographerTemplate(data) {
         h3.setAttribute("aria-label", city + ", " + country);
         aTagline.setAttribute("aria-label", tagline);
         aPrice.setAttribute("aria-label", price + "€/jour");
+        lien.setAttribute("href", url);
+        lien.setAttribute("aria-label", "lien vers la page du photographe" + " " + name);
 
         //Texte afficher
         h2.textContent = name;
@@ -35,5 +40,18 @@ function photographerTemplate(data) {
 
         return article;
     }
-    return { name, picture, city, country, tagline, price, portrait, getUserCardDOM };
+
+    function getPhotographerDom() {
+        //creation DOM
+        const header = document.createElement("div");
+        const photographeCard = document.createElement("div");
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+
+        header.appendChild(photographeCard);
+        header.appendChild(img);
+        console.log("caca");
+        return header;
+    }
+    return { name, picture, city, country, tagline, price, portrait, getUserCardDOM, getPhotographerDom };
 }
